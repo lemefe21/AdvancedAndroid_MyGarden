1d4aba9d87846e85b25bdae7a4b4159a2da5be38
lemefe21 <lemefe21@gmail.com>
2019-03-10 11:36:25 -0300

    03 widgets exercise solved

--- app/src/main/java/com/example/android/mygarden/PlantWateringService.java
+++ app/src/main/java/com/example/android/mygarden/PlantWateringService.java
@@ -17,9 +17,12 @@ package com.example.android.mygarden;
 */
 
 import android.app.IntentService;
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.provider.Settings;
 
@@ -36,9 +39,11 @@ import static com.example.android.mygarden.provider.PlantContract.PATH_PLANTS;
 public class PlantWateringService extends IntentService {
 
     public static final String ACTION_WATER_PLANTS = "com.example.android.mygarden.action.water_plants";
-    // TODO (3): Create a new action ACTION_UPDATE_PLANT_WIDGETS to handle updating widget UI and
+    // OK (3): Create a new action ACTION_UPDATE_PLANT_WIDGETS to handle updating widget UI and
     // implement handleActionUpdatePlantWidgets to query the plant closest to dying and call
     // updatePlantWidgets to refresh widgets
+
+    public static final String ACTION_UPDATE_PLANTS_WIDGET = "com.example.android.mygarden.action.update_plant_widget";
 
 
     public PlantWateringService() {
@@ -57,6 +62,12 @@ public class PlantWateringService extends IntentService {
         context.startService(intent);
     }
 
+    public static void startActionUpdatePlantWidget(Context context) {
+        Intent intent = new Intent(context, PlantWateringService.class);
+        intent.setAction(ACTION_WATER_PLANTS);
+        context.startService(intent);
+    }
+
     /**
      * @param intent
      */
@@ -66,6 +77,8 @@ public class PlantWateringService extends IntentService {
             final String action = intent.getAction();
             if (ACTION_WATER_PLANTS.equals(action)) {
                 handleActionWaterPlants();
+            } else if(ACTION_UPDATE_PLANTS_WIDGET.equals(action)) {
+                handleActionUpdatePlantWidgets();
             }
         }
     }
@@ -86,4 +99,37 @@ public class PlantWateringService extends IntentService {
                 PlantContract.PlantEntry.COLUMN_LAST_WATERED_TIME+">?",
                 new String[]{String.valueOf(timeNow - PlantUtils.MAX_AGE_WITHOUT_WATER)});
     }
+
+    private void handleActionUpdatePlantWidgets() {
+
+        Uri PLANTS_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_PLANTS).build();
+        Cursor cursor = getContentResolver().query(PLANTS_URI, null, null, null, PlantContract.PlantEntry.COLUMN_LAST_WATERED_TIME);
+
+        int imageRes =  R.drawable.grass;
+
+        if(cursor != null && cursor.getCount() > 1) {
+
+            int createTimeIndex = cursor.getColumnIndex(PlantContract.PlantEntry.COLUMN_CREATION_TIME);
+            int waterTimeIndex = cursor.getColumnIndex(PlantContract.PlantEntry.COLUMN_LAST_WATERED_TIME);
+            int planTypeIndex = cursor.getColumnIndex(PlantContract.PlantEntry.COLUMN_PLANT_TYPE);
+
+            long timeNow = System.currentTimeMillis();
+
+            long createdAt = cursor.getLong(createTimeIndex);
+            long wateredAt = cursor.getLong(waterTimeIndex);
+            int planType = cursor.getInt(planTypeIndex);
+
+            cursor.close();
+
+            imageRes = PlantUtils.getPlantImageRes(this, timeNow - createdAt, timeNow - wateredAt, planType);
+
+        }
+
+        AppWidgetManager widgetManager = AppWidgetManager.getInstance(this);
+        int[] appWidgetIds = widgetManager.getAppWidgetIds(new ComponentName(this, PlantWidgetProvider.class));
+
+        PlantWidgetProvider.updatePlantwidgets(this, widgetManager, imageRes, appWidgetIds);
+
+    }
+
 }
